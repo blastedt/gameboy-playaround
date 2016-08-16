cdbb2adb0c6ce93319c7935346557106051ef9fe
blastedt blastedt <blastedt blastedt>
2016-08-16 11:17:33 -0400

    Image to Tile - Convert 8x8 image to z80 assembly

---
+++ Generics/Image to Tile/tilegen.py
@@ -0,0 +1,76 @@
+from __future__ import print_function
+import sys
+def make_byte_list(s):
+	res = []
+	while (len(s) >= 8):
+		res.append( int(s[0:8], 2) )
+		s = s[8:]
+	return res
+
+
+#outputs DB command to make tiles from binary string
+#all tiles have full brightness (second byte echoes)
+#set second byte to $00 for half-bright
+def tilegen(s):
+	byts = make_byte_list(s)
+	print("DB ", end="")
+	count = 0
+	for byt in byts:
+		print("$%X,$%X" % (byt,byt), end="")
+		count += 2
+		if (count < 8):
+			print(",", end="")
+		else:
+			print("\nDB ", end="")
+		count = count % 8
+
+def tilegen_to_string(s):
+	res = ""
+	byts = make_byte_list(s)
+	res += "DB "
+	count = 0
+	for byt in byts:
+		res += "$%X,$%X" % (byt,byt)
+		count += 2
+		if (count < 8):
+			res += ","
+		else:
+			res += "\nDB "
+		count = count % 8
+	res = res[:-4]
+	return res
+
+
+def img_to_tile(img):
+	f = open(img, 'r')
+	s = f.read()
+	binary = ""
+	for c in s:
+		if ord(c) == 0xFF:
+			binary += "0"
+		else:
+			binary += "1"
+	tilegen(binary)
+
+def img_to_binary(img):
+	f = open(img, 'r')
+	s = f.read()
+	binary = ""
+	for c in s:
+		if ord(c) == 0xFF:
+			binary += "0"
+		else:
+			binary += "1"
+	return binary
+
+def main(argv):
+	binary = ""
+	for img in argv:
+		binary += img_to_binary(img)
+	s = tilegen_to_string(binary)
+	print("\n"+s)
+	f = open("tiles.asm", "w")
+	f.write(s)
+	f.close()
+
+main(sys.argv[1:])
---
+++ Generics/Premade tiles/font.asm
@@ -0,0 +1,6 @@
+SECTION "FontTiles", HOME						;Place font somewhere in home bank
+
+FONT_TILES::
+DB $10,$10,$38,$38,$6C,$6C,$C6,$C6
+DB $FE,$FE,$FE,$FE,$C6,$C6,$C6,$C6		;A
+
